042c6c6aa1a9ac47a2c7a1c289b6ae7987cbfaa4
Igor Grebenkov <igor.grebenkov@gmail.com>
2017-03-28 15:32:23 -0400

    can make query for list of foods and display on same page as user.html

---
+++ ajaxtest/display.php
@@ -0,0 +1,4 @@
+<?php
+   echo "Welcome " . $_GET['user_name'] . "<br>" ;
+   echo "Your email id is  " . $_GET['email_id'] ;
+?>
---
+++ ajaxtest/simpleform.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+   <form action="display.php" method="get">
+      Enter your name: <input type="text" name="user_name" /><br>
+      Enter your email id : <input type="text" name="email_id" ><br>
+      <input type="submit" value="Submit"/>
+   </form> 
+</body>
+</html>
